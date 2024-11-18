4330a9df1b0e4ade4ee4cb568d9f1661580d552b
James Harmison <jharmison@redhat.com>
2024-11-18 10:50:58 -0500

    fix: install-config only during install

--- hack/install.sh
+++ hack/install.sh
@@ -31,36 +31,36 @@ fi
 
 hack/hosted-zone.sh
 
-SSH_KEY="$(cat "${INSTALL_DIR}/id_ed25519.pub")"
-export SSH_KEY
-
-templated_variables=(
-	\$PULL_SECRET
-	\$AWS_REGION
-	\$AWS_ACCESS_KEY_ID
-	\$AWS_SECRET_ACCESS_KEY
-	\$CLUSTER_NAME
-	\$BASE_DOMAIN
-	\$SSH_KEY
-	\$WORKER_TYPE
-	\$WORKER_COUNT
-	\$CONTROL_PLANE_TYPE
-	\$CONTROL_PLANE_COUNT
-)
-vars=$(concat_with_comma "${templated_variables[@]}")
-
-envsubst "$vars" <install-config.yaml.tpl >"${INSTALL_DIR}/install-config.yaml"
-
-echo "install-config.yaml:"
-sed 's/^/  /' "${INSTALL_DIR}/install-config.yaml"
-
-set -x
-
 if ${RECOVER_INSTALL}; then
+	set -x
+
 	"${INSTALL_DIR}/openshift-install" --dir "${INSTALL_DIR}" wait-for bootstrap-complete || :
 	"${INSTALL_DIR}/openshift-install" --dir "${INSTALL_DIR}" destroy bootstrap || :
 	"${INSTALL_DIR}/openshift-install" --dir "${INSTALL_DIR}" wait-for install-complete
 	touch "${INSTALL_DIR}/auth/kubeconfig"
 else
+	SSH_KEY="$(cat "${INSTALL_DIR}/id_ed25519.pub")"
+	export SSH_KEY
+
+	templated_variables=(
+		\$PULL_SECRET
+		\$AWS_REGION
+		\$AWS_ACCESS_KEY_ID
+		\$AWS_SECRET_ACCESS_KEY
+		\$CLUSTER_NAME
+		\$BASE_DOMAIN
+		\$SSH_KEY
+		\$WORKER_TYPE
+		\$WORKER_COUNT
+		\$CONTROL_PLANE_TYPE
+		\$CONTROL_PLANE_COUNT
+	)
+	vars=$(concat_with_comma "${templated_variables[@]}")
+	envsubst "$vars" <install-config.yaml.tpl >"${INSTALL_DIR}/install-config.yaml"
+
+	echo "install-config.yaml:"
+	sed 's/^/  /' "${INSTALL_DIR}/install-config.yaml"
+
+	set -x
 	"${INSTALL_DIR}/openshift-install" --dir "${INSTALL_DIR}" create cluster
 fi
