61290a21294b5e76d9ec30d0b48dd82feb916c7e
James Harmison <jharmison@redhat.com>
2025-04-24 15:26:46 -0400

    fix: whitespace stuff

--- hack/update-applications.sh
+++ hack/update-applications.sh
@@ -6,25 +6,25 @@ source hack/common.sh
 # Start with our Makefile- and env-provided apps
 declare -A applications
 for app in $ARGO_APPLICATIONS; do
-	echo "Found $app in ARGO_APPLICATIONS" >&2
-	applications["$app"]=""
+    echo "Found $app in ARGO_APPLICATIONS" >&2
+    applications["$app"]=""
 done
 
 # Include any app we have values or secrets for
 while read -rd $'\0' values; do
-	app="$(basename "$(dirname "$values")")"
-	if [[ ! -v applications["$app"] ]]; then
-		echo "Found $app in values from $values" >&2
-		if [ -e "applications-templates/${app}.yaml.tpl" ]; then
-			applications["$app"]=''
-		else
-			echo "No application template available, skipping..." >&2
-		fi
-	fi
+    app="$(basename "$(dirname "$values")")"
+    if [[ ! -v applications["$app"] ]]; then
+        echo "Found $app in values from $values" >&2
+        if [ -e "applications-templates/${app}.yaml.tpl" ]; then
+            applications["$app"]=''
+        else
+            echo "No application template available, skipping..." >&2
+        fi
+    fi
 done < <(find "${CLUSTER_DIR}" -path '*/values/*' -type f \( -name values.yaml -o -name values.yml -o -name secrets.enc.yaml -o -name secrets.enc.yml \) -print0)
 
 mkdir -p "${CLUSTER_DIR}/applications"
 # Template the apps, update the values references
 for app in "${!applications[@]}"; do
-	/usr/local/bin/openshift-setup app process "applications-templates/${app}.yaml.tpl"
+    /usr/local/bin/openshift-setup app process "applications-templates/${app}.yaml.tpl"
 done
