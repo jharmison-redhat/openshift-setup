3237be2cb86c1bf7a90ba6fbacffadd83071c2af
James Harmison <jharmison@redhat.com>
2025-05-12 18:48:20 -0400

    fix: stop execcing

--- charts/summit-demo/files/database/init.sh
+++ charts/summit-demo/files/database/init.sh
@@ -4,4 +4,4 @@ set -e
 
 export PGPASSWORD="${POSTGRESQL_PASSWORD:-claimdb}"
 
-exec psql -f /opt/app-root/src/postgresql-start/init.sql -U "${POSTGRESQL_USER:-claimdb}" -d "${POSTGRESQL_DATABASE:-claimdb}"
+psql -f /opt/app-root/src/postgresql-start/init.sql -U "${POSTGRESQL_USER:-claimdb}" -d "${POSTGRESQL_DATABASE:-claimdb}"
