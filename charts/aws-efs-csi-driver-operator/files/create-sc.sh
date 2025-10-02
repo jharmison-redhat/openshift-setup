e6518166b99fb227a42e6538ccd91567697dd82e
James Harmison <jharmison@redhat.com>
2025-10-02 15:44:28 -0400

    add: storageclass creation, including dynamic from ACK

---
+++ charts/aws-efs-csi-driver-operator/files/create-sc.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+function target_filesystem {
+    oc get filesystem.efs.services.k8s.aws {{ .Values.storageClass.dynamicFromAck.filesystem.name }} "${@}"
+}
+
+echo -n "Waiting for filesystem to be available"
+while [ $(target_filesystem -ojsonpath='{.status.fileSystemID}') != "available" ]; do
+    sleep 1
+    echo -n .
+done
+echo
+
+echo "Creating StorageClass"
+
+fs_id=$(target_filesystem -ojsonpath='{.status.fileSystemID}')
+cat << EOF | oc apply -f
+$(cat /mnt/create-sc-without-id.yaml)
+  fileSystemId: ${fs_id}
+EOF
