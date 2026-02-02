3ba711eb1d0de6aec9074cf59d7ecd703fc12017
James Harmison <jharmison@redhat.com>
2026-02-02 13:48:03 -0500

    cleanup: remove a bunch of older, unmaintained charts

--- charts/summit-demo/files/database/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export PGPASSWORD="${POSTGRESQL_PASSWORD:-claimdb}"
-
-psql -f /opt/app-root/src/postgresql-start/init.sql -U "${POSTGRESQL_USER:-claimdb}" -d "${POSTGRESQL_DATABASE:-claimdb}"
